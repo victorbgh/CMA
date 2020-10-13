1a57ca40b569d19a3791330badb38e5841de62c1
victorhgb <victorhugo-brito@hotmail.com>
2020-10-13 17:57:47 -0300

    mudanças 13/10

--- index.php
+++ index.php
@@ -92,23 +92,26 @@
                             <div class="row inner">
                                 <div class="col-sm-12">
                                     <div class="image-container ">
-                                        <img class="img-fluid center" src="img/logo-transparente.png" width="auto"
-                                            height="auto" alt="alternative">
+                                        <!-- <img class="img-fluid center" src="img/logo-transparente.png" width="auto"
+                                            height="auto" alt="alternative"> -->
                                         <div class="text-container" style="text-align: center;">
                                             <!-- <p class="p-large"
                                                 style="font-size: 2rem; font-weight: bold; font-style: oblique; line-height: 50px">
                                                 Mais que um escritório de advocacia, uma <span
                                                     class="colorCMA">FAMÍLIA</span></p> -->
 
-                                                <p class="p-large"
+                                                <!-- <p class="p-large"
                                                 style="font-size: 3.5rem; font-family: Brush Script MT; line-height: 50px">
-                                                Mais do que um escritório de advocacia, uma <span class="colorCMA" style="font-family: cursive;">FAMÍLIA!</span></p>
+                                                Mais do que um escritório de advocacia, uma <span class="colorCMA" style="font-family: cursive;">FAMÍLIA!</span></p> -->
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+                    <p class="p-large"
+                    style="font-size: 3.5rem; font-family: Brush Script MT; line-height: 50px; bottom: 100px; width: 100%; position: absolute; text-align: center;">
+                    Mais do que um escritório de advocacia, uma <span class="colorCMA" style="font-family: cursive;">FAMÍLIA!</span></p>
                     <!-- <p class="p-large p-header-class">Mais que um escritório de advocacia, uma <span class="colorCMA">FAMÍLIA</span></p> -->
                 </div>
             </div>
